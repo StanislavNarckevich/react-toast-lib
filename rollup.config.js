7af6ea8c42cc7ae9eb9173bb0db1b3c4c2382c6c
Stanislav Narkevich <azrae2197@gmail.com>
2022-01-30 00:11:18 +0300

    feat: create start config

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import replace from "rollup-plugin-replace";
+import {uglify} from "rollup-plugin-uglify";
+
+const format = process.env.FORMAT === 'cjs' ? 'cjs' : 'iife';
+const env = process.env.NODE_ENV;
+
+export default {
+  input: 'src/index.jsx',
+  output: [
+    {
+      format,
+    }
+  ],
+  plugins: [
+    resolve({
+      extensions: ['.jsx', '.js']
+    }),
+    commonjs(),
+    babel({
+      exclude: './node_modules/**',
+    }),
+    replace({
+      'process.env.NODE_ENV' : JSON.stringify(env || 'development')
+    }),
+    (env === "production" && uglify()),
+  ],
+}
+
