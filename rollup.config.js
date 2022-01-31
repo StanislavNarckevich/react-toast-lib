7ea2085a5941fd05d8f8dadae77d962fb4a014c0
Stanislav Narkevich <azrae2197@gmail.com>
2022-02-01 01:36:26 +0300

    refactor: edit rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,88 @@
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
-import replace from "rollup-plugin-replace";
-import {uglify} from "rollup-plugin-uglify";
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import filesize from 'rollup-plugin-filesize';
+import autoprefixer from 'autoprefixer';
+import externalGlobals from 'rollup-plugin-external-globals';
+// import pkg from './package.json';
 
-const env = process.env.NODE_ENV;
+const INPUT_FILE_PATH = 'src/index.jsx';
+const OUTPUT_NAME = 'Example';
 
-export default {
-  input: 'src/index.jsx',
-  external: [
-    'react',
-    'react-dom',
-    'styled-components'
-  ],
-  output: [
-    {
-      globals: {
-        'react': 'React',
-        'react-dom':'ReactDOM',
-        'styled-components': 'styled',
-      },
-      file: 'build/bundle.cjs.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      globals: {
-        'react': 'React',
-        'react-dom':'ReactDOM',
-        'styled-components': 'styled',
-      },
-      file: 'build/bundle.iife.js',
-      format: 'iife',
-      sourcemap: true,
-    },
+const GLOBALS = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  'styled-components': 'styled',
+};
 
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve({
-      extensions: ['.jsx', '.js']
-    }),
-    commonjs(),
-    babel({
-      exclude: './node_modules/**',
-      plugins: ['babel-plugin-styled-components'],
-    }),
-    replace({
-      'process.env.NODE_ENV' : JSON.stringify(env || 'development')
-    }),
-    (env === "production" && uglify()),
-  ],
-}
+const PLUGINS = [
+  postcss({
+    extract: true,
+    plugins: [
+      autoprefixer,
+    ],
+  }),
+  babel({
+    // babelHelpers: 'runtime',
+    babelHelpers: 'inline',
+    exclude: 'node_modules/**',
+  }),
+  resolve({
+    browser: true,
+    resolveOnly: [
+      /^(?!react$)/,
+      /^(?!react-dom$)/,
+      /^(?!styled-components)/,
+    ],
+  }),
 
+  // externalGlobals({                      // partially solves the problem
+  //   "react": "window.React",
+  //   "react-dom": "window.ReactDOM",
+  //   "styled-components": "window.styled",
+  // }),
+  commonjs(),
+  filesize(),
+];
+
+const EXTERNAL = [
+  'react',
+  'react-dom',
+  'styled-components',
+];
+
+// https://github.com/rollup/plugins/tree/master/packages/babel#babelhelpers
+const CJS_AND_ES_EXTERNALS = EXTERNAL.concat(/@babel\/runtime/);
+
+const OUTPUT_DATA = [
+  {
+    file: 'build/bundle.js',
+    // file: pkg.browser,
+    format: 'umd',
+  },
+  {
+    // file: pkg.main,
+    file: 'build/bundle.js',
+    format: 'cjs',
+  },
+  {
+    file: 'build/bundle.js',
+    // file: pkg.module,
+    format: 'es',
+  },
+];
+
+const config = OUTPUT_DATA.map(({ file, format }) => ({
+  input: INPUT_FILE_PATH,
+  output: {
+    file,
+    format,
+    name: OUTPUT_NAME,
+    globals: GLOBALS,
+  },
+  external: ['cjs', 'es'].includes(format) ? CJS_AND_ES_EXTERNALS : EXTERNAL,
+  plugins: PLUGINS,
+}));
+
+export default config;
