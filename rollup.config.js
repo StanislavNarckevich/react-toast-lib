7e51cf37f184a10b1a47f4e87fdfe29d8f4de61f
Stanislav Narkevich <azrae2197@gmail.com>
2022-02-02 00:49:24 +0300

    fix: edit rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,88 +1,41 @@
-import babel from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import postcss from 'rollup-plugin-postcss';
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import replace from "rollup-plugin-replace";
+import {uglify} from "rollup-plugin-uglify";
 import filesize from 'rollup-plugin-filesize';
-import autoprefixer from 'autoprefixer';
-import externalGlobals from 'rollup-plugin-external-globals';
-// import pkg from './package.json';
 
-const INPUT_FILE_PATH = 'src/index.jsx';
-const OUTPUT_NAME = 'Example';
+const env = process.env.NODE_ENV;
 
-const GLOBALS = {
-  react: 'React',
-  'react-dom': 'ReactDOM',
-  'styled-components': 'styled',
-};
+export default {
+  input: 'src/index.jsx',
+  output: [
+    {
+      file: 'build/bundle.js',
+      format: 'cjs',
+    }
+  ],
+  plugins: [
+    resolve({
+      extensions: ['.jsx', '.js']
+    }),
+    commonjs({
+      ignoreGlobal: true,
+      include: /\/node_modules\//,
+      namedExports: {
+        react: Object.keys(require('react')),
+        'react-is': Object.keys(require('react-is')),
+      },
+    }),
+    babel({
+      exclude: './node_modules/**',
+      plugins: ['babel-plugin-styled-components'],
+    }),
+    replace({
+      'process.env.NODE_ENV' : JSON.stringify(env || 'development')
+    }),
+    filesize(),
+    (env === "production" && uglify()),
+  ],
+}
 
-const PLUGINS = [
-  postcss({
-    extract: true,
-    plugins: [
-      autoprefixer,
-    ],
-  }),
-  babel({
-    // babelHelpers: 'runtime',
-    babelHelpers: 'inline',
-    exclude: 'node_modules/**',
-  }),
-  resolve({
-    browser: true,
-    resolveOnly: [
-      /^(?!react$)/,
-      /^(?!react-dom$)/,
-      /^(?!styled-components)/,
-    ],
-  }),
-
-  // externalGlobals({                      // partially solves the problem
-  //   "react": "window.React",
-  //   "react-dom": "window.ReactDOM",
-  //   "styled-components": "window.styled",
-  // }),
-  commonjs(),
-  filesize(),
-];
-
-const EXTERNAL = [
-  'react',
-  'react-dom',
-  'styled-components',
-];
-
-// https://github.com/rollup/plugins/tree/master/packages/babel#babelhelpers
-const CJS_AND_ES_EXTERNALS = EXTERNAL.concat(/@babel\/runtime/);
-
-const OUTPUT_DATA = [
-  {
-    file: 'build/bundle.js',
-    // file: pkg.browser,
-    format: 'umd',
-  },
-  {
-    // file: pkg.main,
-    file: 'build/bundle.js',
-    format: 'cjs',
-  },
-  {
-    file: 'build/bundle.js',
-    // file: pkg.module,
-    format: 'es',
-  },
-];
-
-const config = OUTPUT_DATA.map(({ file, format }) => ({
-  input: INPUT_FILE_PATH,
-  output: {
-    file,
-    format,
-    name: OUTPUT_NAME,
-    globals: GLOBALS,
-  },
-  external: ['cjs', 'es'].includes(format) ? CJS_AND_ES_EXTERNALS : EXTERNAL,
-  plugins: PLUGINS,
-}));
-
-export default config;
--- src/components/Toast/style.js
+++ src/components/Toast/style.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 const ToastContainer = styled.div`
   width: 360px;
-  border-radius: 40px;
-  height: 100px;
+  border-radius: 15px;
+  height: 125px;
   background-color: #9A40D3;
 `;
 
